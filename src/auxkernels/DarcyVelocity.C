d4cbc08451bd93910e9743ee9d6b6460cd326ae0
Stu Blair <stu314159@gmail.com>
2023-05-18 15:26:03 -0400

    added aux variable and associated test per step 10

---
+++ src/auxkernels/DarcyVelocity.C
@@ -0,0 +1,32 @@
+#include "DarcyVelocity.h"
+
+#include "metaphysicl/raw_type.h"
+
+registerMooseObject("PhoenixApp",DarcyVelocity);
+
+InputParameters
+DarcyVelocity::validParams()
+{
+    InputParameters params = VectorAuxKernel::validParams();
+    params.addClassDescription("Compute volumetric flux (which has units of velocity) for a given "
+        "pressure gradient, premeability, and viscosity using Darcy's Law: "
+        "$\\vec{u} = -\\frac{\\mathbf{K}}{\\mu} \\nabla p$");
+    params.addRequiredCoupledVar("pressure", "The pressure field.");
+
+    return params;
+}
+
+DarcyVelocity::DarcyVelocity(const InputParameters & parameters)
+: VectorAuxKernel(parameters),
+_grad_p(coupledGradient("pressure")),
+_permeability(getADMaterialProperty<Real>("permeability")),
+_viscosity(getADMaterialProperty<Real>("viscosity"))
+{
+
+}
+
+RealVectorValue
+DarcyVelocity::computeValue()
+{
+    return -MetaPhysicL::raw_value(_permeability[_qp] / _viscosity[_qp])*_grad_p[_qp];
+}
