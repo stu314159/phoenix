ef3c6dc5c5887b6a8d98828e29e6f2684e0dbee1
Stu Blair <stu314159@gmail.com>
2023-05-18 09:52:00 -0400

    added step 6 modifications and test input file

---
+++ problems/step6/step6.i
@@ -0,0 +1,54 @@
+[Mesh]
+    type = GeneratedMesh 
+    dim = 2
+    nx = 100
+    ny = 10
+    xmax = 0.304
+    ymax = 0.0257
+[]
+
+[Problem]
+    type = FEProblem
+    coord_type = RZ
+    rz_coord_axis = X
+[]
+
+[Variables]
+    [pressure]
+
+    []
+[]
+
+[Kernels]
+    [diffusion]
+        type = DarcyPressure
+        variable = pressure
+        permeability = 0.8451e-09 # (m^2) assumed permeability of the porous medium
+    []
+[]
+
+[BCs]
+    [inlet]
+        type = ADDirichletBC
+        variable = pressure
+        boundary = left
+        value = 4000 # (Pa)
+    []
+    [outlet]
+        type = ADDirichletBC
+        variable = pressure
+        boundary = right
+        value = 0
+    []
+[]
+
+[Executioner]
+    type = Steady
+    solve_type = NEWTON
+    petsc_options_iname = '-pc_type -pc_hypre_type'
+    petsc_options_value = 'hypre boomeramg'
+[]
+
+[Outputs]
+    exodus = true
+[]
--- src/kernels/DarcyPressure.C
+++ src/kernels/DarcyPressure.C
@@ -6,15 +6,24 @@ InputParameters
 DarcyPressure::validParams()
 {
     auto params = ADKernelGrad::validParams();
-    params.addClassDescription("Same 'Diffusion' in terms of physics/residual, but the Jacobian "
-    "is computed using forward automatic differentiation");
+    params.addClassDescription("Compute the diffusion term for Darcy pressure ($p$) equation: "
+                           "$-\\nabla \\cdot \\frac{\\mathbf{K}}{\\mu} \\nabla p = 0$");
+    
+    params.addRequiredParam<Real>("permeability","The isotropic permeability ($K$) of the medium");
+
+    params.addParam<Real>(
+        "viscosity",
+        7.98e-04,
+        "The dynamic viscosity ($\\mu$) of the fluid, the default value is that of water at 30 "
+        "degrees Celcius (7.98e-04 Pa-s).");
+    
     return params;
 }
 
 DarcyPressure::DarcyPressure(const InputParameters & parameters) : 
 ADKernelGrad(parameters),
-_permeability(0.8451e-09),
-_viscosity(7.98e-04)
+_permeability(getParam<Real>("permeability")),
+_viscosity(getParam<Real>("viscosity"))
 {
 
 }
