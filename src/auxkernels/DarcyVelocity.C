37b6b6499f09a8ecd495aefe94338772d71451b6
Stu Blair <stu314159@gmail.com>
2023-06-07 10:41:46 -0600

    clean up after toying with DarcyVeclocity.C

--- src/auxkernels/DarcyVelocity.C
+++ src/auxkernels/DarcyVelocity.C
@@ -2,31 +2,31 @@
 
 #include "metaphysicl/raw_type.h"
 
-registerMooseObject("PhoenixApp",DarcyVelocity);
+registerMooseObject("PhoenixApp", DarcyVelocity);
 
 InputParameters
 DarcyVelocity::validParams()
 {
-    InputParameters params = VectorAuxKernel::validParams();
-    params.addClassDescription("Compute volumetric flux (which has units of velocity) for a given "
-        "pressure gradient, premeability, and viscosity using Darcy's Law: "
-        "$\\vec{u} = -\\frac{\\mathbf{K}}{\\mu} \\nabla p$");
-    params.addRequiredCoupledVar("pressure", "The pressure field.");
+  InputParameters params = VectorAuxKernel::validParams();
+  params.addClassDescription("Compute volumetric flux (which has units of velocity) for a given "
+                             "pressure gradient, premeability, and viscosity using Darcy's Law: "
+                             "$\\vec{u} = -\\frac{\\mathbf{K}}{\\mu} \\nabla p$");
+  params.addRequiredCoupledVar("pressure", "The pressure field.");
 
-    return params;
+  return params;
 }
 
 DarcyVelocity::DarcyVelocity(const InputParameters & parameters)
-: VectorAuxKernel(parameters),
-_grad_p(coupledGradient("pressure")),
-_permeability(getADMaterialProperty<Real>("permeability")),
-_viscosity(getADMaterialProperty<Real>("viscosity"))
+  : VectorAuxKernel(parameters),
+    _grad_p(coupledGradient("pressure")),
+    _permeability(getADMaterialProperty<Real>("permeability")),
+    _viscosity(getADMaterialProperty<Real>("viscosity"))
 {
-
 }
 
 RealVectorValue
 DarcyVelocity::computeValue()
 {
-    return -MetaPhysicL::raw_value(_permeability[_qp] / _viscosity[_qp])*_grad_p[_qp];
+  return -MetaPhysicL::raw_value(_permeability[_qp] / _viscosity[_qp]) * _grad_p[_qp];
+  // return -(_permeability[_qp] / _viscosity[_qp]) * _grad_p[_qp];
 }
